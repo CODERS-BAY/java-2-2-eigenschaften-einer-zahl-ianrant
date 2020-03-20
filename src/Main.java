bdb5979e99e7e5f05e757ee2f5f15e09c5dcc850
Ben Leitl <ben.leitl@me.com>
2020-03-20 11:58:31 +0100

    Implement feedback; Simplified boolean initialization by removing unnecessary if-statements

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,21 @@ public class Main {
         int scannedNumber = 0;
         int luckyNumber = (int) (Math.random()*10) * (int) (Math.random()*10);
 
-        boolean isRound = false;
-        boolean isEven = false;
-        boolean isLuckyNumber = false;
-        boolean hasTwoDecimals = false;
-
         System.out.println("Pssst... my lucky number is "+luckyNumber);
         System.out.println("Please enter an integer:");
         scannedNumber = scanner.nextInt();
 
+        boolean isRound = scannedNumber % 10 == 0;
+        boolean isEven = scannedNumber % 2 == 0;
+        boolean isLuckyNumber = scannedNumber == luckyNumber;
+        boolean hasTwoDecimals = scannedNumber < 100 && scannedNumber > 10;
+
         System.out.println();
-        if (scannedNumber % 10 == 0) {
-            isRound = true;
-        }
 
-        if (scannedNumber % 2 == 0) {
-            isEven = true;
-        }
-
-        if (scannedNumber == luckyNumber) {
-            isLuckyNumber = true;
-        }
-
-        if (scannedNumber < 100 && scannedNumber > 10) {
-            hasTwoDecimals = true;
-        }
-
-        String round = (isRound == true) ? "is round, " : "is not round, ";
-        String even = (isEven == true) ? "is even, " : "is odd, ";
-        String lucky = (isLuckyNumber == true) ? "equals your lucky number, " : "differs from your lucky number, ";
-        String decimals = (hasTwoDecimals == true) ? "and is has two decimals." : "and it does not have two decimals.";
+        String round = isRound ? "is round, " : "is not round, ";
+        String even = isEven ? "is even, " : "is odd, ";
+        String lucky = isLuckyNumber ? "equals your lucky number, " : "differs from your lucky number, ";
+        String decimals = hasTwoDecimals ? "and is has two decimals." : "and it does not have two decimals.";
 
         System.out.print("The result are in: The number you entered ");
         System.out.print(round);
